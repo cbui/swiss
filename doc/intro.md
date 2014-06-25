d7d629a1c22bbb1f3cd56d0807cbfca0a4f62134
Chris Bui <chris@weare.ninja>
2014-06-25 02:24:02 -0500

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to swiss
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/swiss/core_test.clj
@@ -0,0 +1,7 @@
+(ns swiss.core-test
+  (:require [clojure.test :refer :all]
+            [swiss.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
