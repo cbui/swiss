274aae1a5f385f6001f20bec9d673cf816511047
Chris Bui <chris@weare.ninja>
2014-08-21 22:35:22 -0500

    Starting rewrite. Add compress-javascript function that returns a string

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [digest "1.4.4"]
+                 [me.raynes/fs "1.4.4"]
                  [com.yahoo.platform.yui/yuicompressor "2.4.7"]]
   :scm {:name "git"
         :url "https://github.com/Christopher-Bui/swiss"})
--- src/swiss/core.clj
+++ src/swiss/core.clj
@@ -2,60 +2,60 @@
   (:require [clojure.java.shell :refer [sh]]
             [clojure.string :refer [join]]
             [digest :refer [md5]]
-            [clojure.java.io :refer [as-file]])
-  (:import com.yahoo.platform.yui.compressor.YUICompressor))
+            [clojure.java.io :refer [as-file]]
+            [me.raynes.fs :refer [glob]])
+  (:import [com.yahoo.platform.yui.compressor JavaScriptCompressor]
+           [java.nio.file FileSystems]
+           [java.io InputStreamReader FileInputStream StringWriter BufferedOutputStream]))
 
-(defn md5-file [file-path]
-  (md5 (as-file file-path)))
+(defn compress-javascript [input-file &
+                           {:keys
+                            [disable-optimizations?
+                             preserve-semi?
+                             nomunge?
+                             line-break
+                             charset
+                             verbose?]
+                            :or
+                            {disable-optimizations? false
+                             verbose? false
+                             preserve-semi? false
+                             nomunge? false
+                             line-break -1
+                             charset "utf-8"}}]
+  (let [compressor (JavaScriptCompressor.
+                    (InputStreamReader.
+                     (FileInputStream. input-file)
+                     charset)
+                    nil)
+        writer (StringWriter.)]
+    (.compress
+     compressor
+     writer
+     line-break
+     nomunge?
+     verbose?
+     preserve-semi?
+     disable-optimizations?)
+    (.toString (.getBuffer writer))))
 
-(defn- get-current-working-directory []
-  (System/getProperty "user.dir"))
-
-(defn execute-compressor [args]
-  (YUICompressor/main (into-array String args)))
-
-(defn build-command [args]
-  (filter #(not (nil? %)) (flatten args)))
-
-(defn compress-javascript [input-file output-file & [{:keys [disable-optimizations? preserve-semi? nomunge? line-break? charset verbose?] :as options}]]
-  (let [current-working-directory (get-current-working-directory)
-        args ["--type"
-              "js"
-              (when nomunge?
-                "--nomunge")
-              (when preserve-semi?
-                "--preserve-semi")
-              (when disable-optimizations?
-                "--disable-optimizations")
-              (when line-break?
-                "--linebreak")
-              (when charset
-                ["--charset" charset])
-              (when verbose?
-                "-v")
-              (str current-working-directory "/" input-file)
-              "-o"
-              (str current-working-directory "/" output-file)]]
-    (execute-compressor (build-command args))))
-
-
-(defn compress-stylesheet [input-file output-file & [{:keys [line-break? charset verbose?] :as options}]]
-  (let [current-working-directory (get-current-working-directory)
-        args ["--type"
-              "css"
-              (when line-break?
-                "--linebreak")
-              (when charset
-                ["--charset" charset])
-              (when verbose?
-                "-v")
-              (str current-working-directory "/" input-file)
-              "-o"
-              (str current-working-directory "/" output-file)]]
-    (execute-compressor (build-command args))))
 
 (defn concatenate-asset-to-output [asset]
   "Takes in an asset, which is a map with a :concatenate and
 an :output key. Takes the files to be concatenated and concatenates
 them together, outputting to the :output file."
   (spit (:output asset) (apply str (map slurp (:concatenate asset)))))
+
+
+(defn src [files]
+  {:src files})
+
+(defn compress-js [swiss-map yui-options]
+  (merge swiss-map {}))
+
+#_(-> (src ["test/assets/test.js" "test/assets/jquery.js"])
+      (uglify)
+      (concat "all.min.js")
+      (output))
+
+;;(compress-javascript "test/assets/test.js" nil)
